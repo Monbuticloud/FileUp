3ac6a311ec514d3d47e5029cc6f73b2439b81ef4
Monbuticloud <145574857+Monbuticloud@users.noreply.github.com>
2024-03-24 11:54:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ function uploadFile() {
     const file = fileInput.files[0];
     if (file) {
         // You can handle the file here (e.g., upload to a server, process, etc.)
+        // https://github.com/Monbuticloud/FileUp.git
+        git remote add origin https://github.com/Monbuticloud/FileUp.git
+        git pull
+        git add file
+        git commit -m "Add new file"
+        git push -u origin main
+        
         outputDiv.innerHTML = `File uploaded: ${file.name}`;
     } else {
         outputDiv.innerHTML = 'Please select a file.';
